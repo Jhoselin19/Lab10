cf733df90a88bc75e6b3b8f0fd6ff3b4487eaf8a
jhoselin19 <jhoselin.ponce@tecsup.edu.pe>
2023-10-30 23:10:04 -0500

    first commit

---
+++ src/app/pages/users/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/users';
+import { DataLoginService } from 'src/app/services/data-login.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+
+export class LoginComponent {
+  
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private dataLogin: DataLoginService){
+      this.loginForm = this.fb.group(
+        {
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        }
+      )
+  }
+
+  loginUser(){
+
+    const username = this.loginForm.get('username')?.value;
+    const password = this.loginForm.get('password')?.value;
+
+    if (this.dataLogin.validarCredenciales(username, password)) {
+      this.dataLogin.nombreUsuario = username;
+      console.log(this.dataLogin.nombreUsuario);
+      this.router.navigate(['/listar-productos']);
+    } else {
+      // El usuario no existe o las credenciales ingresadas son incorrectas
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Usuario o contraseña incorrectos',
+      });
+    }
+
+  }
+
+}
+
+
